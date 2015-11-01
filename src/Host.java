ea36606a7a363fa10777e7675aa14846656c8a71
Nailen Matschke <nailen.matschke@gmail.com>
2015-11-01 14:49:38 -0800

    Did some directory restructuring, we don't need to worry about package stuff yet

--- src/Host.java
+++ src/Host.java
@@ -1,19 +1,12 @@
-package com.nirkhe;
-
-/**
- * Created by chinmay on 11/1/15.
- */
 public class Host {
 
     private int address;
     private Link link;
     private int link_buffer_size;
 
-    public Host(int address, Link link, int link_buffer_size)
-    {
+    public Host(int address, Link link, int link_buffer_size) {
         this.address = address;
         this.link = link;
         this.link_buffer_size = link_buffer_size;
     }
-
 }
