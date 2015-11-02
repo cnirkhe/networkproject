4a7e80f73b4c9ed5e92233a98811b3a739ed67c2
Parker Won <pwon111@gmail.com>
2015-11-01 17:15:34 -0800

    Link.java complete for current design

--- src/Link.java
+++ src/Link.java
@@ -1,12 +1,25 @@
 public class Link {
     private int capacity;               // bits per milli-second
     private float transmissionDelay;    // milli-seconds
-    //
+
+    private Node connection_0;
+    private Node connection_1;
+    
+    // constructors
     public Link(int capacity, float transmissionDelay) {
         this.capacity = capacity;
         this.transmissionDelay = transmissionDelay;
+        connection_0 = null;
+        connection_1 = null;
+    }
+    public Link(int capacity, float transmissionDelay, Node node0, Node node1) {
+        this.capacity = capacity;
+        this.transmissionDelay = transmissionDelay;
+        connection_0 = node0;
+        connection_1 = node1;
     }
 
+    // private variable accessors and modifiers
     public int getCapacity() {
         return this.capacity;
     }
@@ -18,5 +31,27 @@ public class Link {
     }
     public void setTransmissionDelay() {
         this.transmissionDelay = transmissionDelay;
+    }
+
+    // connection functionality
+    public boolean connect(Node node) {
+        if (connection_0 == null) {
+            connection_0 = node;
+            return true;
+        } else if (connection_1 == null) {
+            connection_1 = node;
+            return true;
+        }
+        return false;
+    }
+    public boolean disconnect(Node node) {
+        if (connection_0 == node) {
+            connection_0 = null;
+            return true;
+        } else if (connection_1 == node) {
+            connection_1 = null;
+            return true;
+        }
+        return false;
     }
 }
