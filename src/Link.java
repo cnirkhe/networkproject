be30ad8c4ec5c626d4d65722e475088b0bc14cd9
Parker Won <pwon111@gmail.com>
2015-11-01 16:12:06 -0800

    forgot to git add Link.java in prev commit. oops. fixed

--- src/Link.java
+++ src/Link.java
@@ -1,3 +1,22 @@
 public class Link {
-    
+    private int capacity;               // bits per milli-second
+    private float transmissionDelay;    // milli-seconds
+    //
+    public Link(int capacity, float transmissionDelay) {
+        this.capacity = capacity;
+        this.transmissionDelay = transmissionDelay;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+    public float getTransmissionDelay() {
+        return this.transmissionDelay;
+    }
+    public void setCapacity() {
+        this.capacity = capacity;
+    }
+    public void setTransmissionDelay() {
+        this.transmissionDelay = transmissionDelay;
+    }
 }
